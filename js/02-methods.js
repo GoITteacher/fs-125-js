f8acbf354b60a9e300d2ff4d740e1f3cff401b0a
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2026-01-23 21:56:47 +0200

    update

--- js/02-methods.js
+++ js/02-methods.js
@@ -5,15 +5,47 @@
  * - Доступ до властивостей об'єкта через this
  * - Зміна за посиланням
  */
+//!=========================================
 
+const cat = {
+  name: 'Marsik',
+  age: 2,
+  color: 'black',
+  breed: 'Noname',
+
+  sayMeow() {
+    console.log('Meow, meow');
+  },
+  eat(food) {
+    console.log(`Ням ням ${food} смакує`);
+  },
+  sleep() {
+    console.log('Zzzzz');
+  },
+};
+
+//!=========================================
 const playlist = {
-  name: "My amazing playlist",
+  name: 'My amazing playlist',
   rating: 5,
-  tracks: ["track-1", "track-2", "track-3"],
-  changeName(newName) {},
-  addTrack(track) {},
-  updateRating(newRating) {},
-  getTrackCount() {},
+  tracks: ['track-1', 'track-2', 'track-3'],
+
+  changeName(newName) {
+    playlist.name = newName;
+  },
+  addTrack(track) {
+    playlist.tracks.push(track);
+  },
+  updateRating(newRating) {
+    playlist.rating = newRating;
+  },
+  getTrackCount() {
+    return playlist.tracks.length;
+  },
+
+  play() {},
+  pause() {},
+  removeTrack() {},
 };
 
 // console.log(playlist.getTrackCount());
@@ -28,3 +60,69 @@ const playlist = {
 
 // playlist.updateRating(4);
 // console.log(playlist);
+
+//!=========================================
+
+const student = {
+  firstname: 'Vasya',
+  lastname: 'Petrenko',
+  age: 20,
+
+  changeInfo() {},
+  updateFirstname() {},
+  updateLastname() {},
+  updateAge() {},
+};
+
+// const student = {
+//   hp: 100,
+//   damage: 51,
+//   speed: 20,
+
+//   kick() {},
+//   hide() {},
+//   run() {},
+// };
+
+//!=========================================
+
+// const user = {
+//   fistname: 'Vasya',
+//   lastname: 'Petrenko',
+//   age: 25,
+
+//   showInfo() {
+//     const info = `${this.fistname} ${this.lastname} (${this.age})`;
+//     console.log(info);
+//   },
+
+//   sum(x, y) {
+//     console.log('SUM:', x + y);
+//     return x + y;
+//   },
+// };
+
+// user.sum(10, 30);
+
+// const user2 = {
+//   fistname: 'Vasya',
+//   lastname: 'Petrenko',
+//   age: 25,
+
+//   showFullName() {
+//     console.log('Fullname:', this.fistname, this.lastname);
+//   },
+
+//   showAge() {
+//     console.log('Age:', this.age);
+//   },
+
+//   showInfo() {
+//     this.showFullName();
+//     this.showAge();
+//   },
+// };
+
+// user2.showInfo();
+
+// const res = Math.round(4.5)
--- js/03-spread-rest.js
+++ js/03-spread-rest.js
@@ -8,8 +8,50 @@
  * - Створення об'єкта
  */
 
-const numbers = [];
-// console.log(numbers);
+//!=========================================
+// const numbers = [1, 3, 5, 1, 2, 4, 6, 7, 2];
+
+// const copy = [...numbers];
+
+// console.log(copy);
+
+//!=========================================
+
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+// const arr3 = [7, 8, 9];
+
+// const copy = ['START', ...arr2, ...arr1, ...arr3, 'END'];
+
+// console.log(copy);
+
+//!=========================================
+
+// const arr = [1, 2, 3];
+// arr.push(4);
+
+// let arr = [1, 2, 3, 4];
+// const copy = [...arr, 5]
+
+//!=========================================
+
+// const arr = [1, 3, 45, 1, 2, 4, 52];
+// Math.max(...arr);
+
+//!=========================================
+
+// const user = {
+//   age: 10,
+//   name: 'vasya',
+// };
+
+// const copy = { ...user, balance: 100 };
+
+// console.log(copy);
+
+//!=========================================
+
+//!=========================================
 
 /**
  * Пошук найменшої аьо найбільшої температури (числа)
@@ -48,7 +90,7 @@ const objC = {};
  * Оновлюємо налаштування користувача
  */
 const defaultSettings = {
-  theme: "light",
+  theme: 'light',
   showNotifications: true,
   hideSidebar: false,
 };
@@ -61,3 +103,27 @@ const userSettings = {
 const finalSettings = {};
 
 // console.log(finalSettings);
+
+//!=========================================
+
+// function foo(min, ...items) {
+//   for (const item of items) {
+//     if (item > min) {
+//       console.log(item);
+//     }
+//   }
+// }
+
+function splice(arr, index, count, ...args) {
+  // ['Hello', 'World', 'TEST']
+  const part1 = arr.slice(0, index);
+  const part2 = arr.slice(index + count);
+
+  const res = [...part1, ...args, ...part2];
+  console.log(res);
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// [1, 2, 'Hello', 'World', 'TEST', 6, 7, 8, 9]
+splice(arr, 2, 3, 'Hello', 'World', 'TEST');
