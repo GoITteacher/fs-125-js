549c56ce72ab84eac417a95546ae3ac6b82a2bff
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2026-02-25 20:56:53 +0200

    update

--- js/01-localstorage.js
+++ js/01-localstorage.js
@@ -1,36 +1,95 @@
 /**
  * LocalStorage
  */
+//!=========================================
 
-const LS_KEY = "Array of names";
-const names = ["Alice", "Kate", "Emma"];
+// const x = {
+//   name: 'Vasya',
+//   age: 25,
+// };
+
+// const zip = JSON.stringify(x);
+
+// console.log(zip);
+
+//!=========================================
+
+// const zip = '[1, 2, 3, "hello", null]';
+
+// const arr = JSON.parse(zip);
+
+// console.log(arr);
+
+//!=========================================
+// const LS_KEY = "Array of names";
+// const names = ["Alice", "Kate", "Emma"];
 
 /**
  * Збереження
  * Чому треба використовувати метод JSON.stringify
  */
+//!=========================================
+
+// const x = {
+//   name: 'Vasya',
+//   age: undefined,
+//   showInfo() {
+//     console.log(this.name);
+//   },
+// };
+// const zip = JSON.stringify(x);
+// localStorage.setItem('userData', zip);
+
+//!=========================================
+
+//!=========================================
 
 /**
  * Читання
  * Чому треба використовувати метод JSON.parse
  */
 
+// const zip = localStorage.getItem('userData');
+// const data = JSON.parse(zip);
+// console.log(data);
+
 /**
  * Видалення
  */
+
+// localStorage.removeItem('userData');
+// localStorage.removeItem('userName');
 
 /**
  * LocalStorage не може зберігати функції
  */
 
-function add(a, b) {
-  return a + b;
+// function add(a, b) {
+//   return a + b;
+// }
+
+// const calculator = {
+//   a: 5,
+//   b: 10,
+//   add() {
+//     return this.a + this.b;
+//   },
+// };
+
+//!=========================================
+
+console.log('START');
+
+const arr = ['html', 'css', 'js'];
+const index = Number(prompt('Enter index'));
+const elem = arr[index];
+
+try {
+  const str = elem.toUpperCase();
+  console.log(str);
+} catch (err) {
+  console.log(err);
+  console.log('Щось пішло не так');
 }
 
-const calculator = {
-  a: 5,
-  b: 10,
-  add() {
-    return this.a + this.b;
-  },
-};
+console.log('END');
--- js/02-feedback.js
+++ js/02-feedback.js
@@ -1,20 +1,38 @@
-const STORAGE_KEY = "feedback-msg";
+const STORAGE_KEY = 'feedback-msg';
 
-const form = document.querySelector(".feedback-form");
-const textarea = form.querySelector("textarea");
+const form = document.querySelector('.feedback-form');
+const textarea = form.querySelector('textarea');
 
-/*
- * - Скасовуємо стандартну поведінку
- * - Видаляємо повідомлення зі сховища
- * - Очищуємо форму
- */
+form.addEventListener('input', e => {
+  const formData = new FormData(form);
 
-/*
- * - Отримуємо значення поля
- * - Зберігаємо його у сховище
- */
+  const obj = {
+    name: formData.get('name'),
+    message: formData.get('message'),
+  };
 
-/*
- * - Отримуємо значення зі сховища
- * - Якщо там щось було, оновлюємо DOM
- */
+  const zip = JSON.stringify(obj);
+  localStorage.setItem(STORAGE_KEY, zip);
+});
+
+document.addEventListener('DOMContentLoaded', e => {
+  const zip = localStorage.getItem(STORAGE_KEY);
+  const userForm = JSON.parse(zip) || {};
+  form.elements.name.value = userForm.name || '';
+  form.elements.message.value = userForm.message || '';
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(form);
+
+  const obj = {
+    name: formData.get('name'),
+    message: formData.get('message'),
+  };
+
+  console.log(obj);
+
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+});
